b7a179398da03cb34326d71bbf2a1eac84810eec
manish sharma dev <manishvsharma1@gmail.com>
2025-02-27 07:53:42 -1200

    wrote routes related to find team data and some ui parts

---
+++ app/api/(Team)/findteambyid/[teamid]/route.ts
@@ -0,0 +1,43 @@
+import { NextResponse } from "next/server";
+import { prisma } from "@/lib/prisma";
+
+export async function GET(req:Request) {
+    try {
+
+        const url = new URL(req.url)
+        const teamid = url.pathname.split('/')[3]
+
+        if(!teamid){
+            return NextResponse.json(
+                {message:'Team id is neccessary'},
+                {status:400}
+            )
+        }
+
+        const teamdata = await prisma.team.findUnique({
+            where : {
+                id : teamid
+            }
+        })
+
+        if(!teamdata){
+            return NextResponse.json(
+                {error:`Invalid team id.`},
+                {status:400}
+            )
+        }
+
+        return NextResponse.json(
+            {message:`Team data fetched.`,data:teamdata},
+            {status:200}
+        )
+
+        
+    } catch (error) {
+        console.log(`Failed To get teamdata: ${error}`)
+        return NextResponse.json(
+            {error :`Failed To get teamdata: ${error}`},
+            {status:500}
+        )
+    }
+}
--- app/dashboard/me/page.tsx
+++ app/dashboard/me/page.tsx
@@ -1,55 +1,53 @@
 "use client"
 import React from 'react'
-import Image from 'next/image'
-import sample from "@/public/sample.jpeg"
 import { DialogDemoTextArea,DialogDemoInput,DialogDemoSelect } from '@/components/EditBox'
 import { useUser } from '@clerk/nextjs'
 
 export default function page() {
   const { user } = useUser()
   return (
-   <div className='px-16 py-10'>
-     <div className='flex flex-col gap-y-3 text-xs'>
-      <p>
-        <span className='text-xl'>Welcome, </span>
-        <span className='text-base'>Manish</span>
-      </p>
+    <div className='px-16 py-10'>
+      <div className='flex flex-col gap-y-3 text-xs'>
+        <p>
+          <span className='text-xl'>Welcome, </span>
+          <span className='text-base'>Manish</span>
+        </p>
         <img src={`${user?.imageUrl}`} alt='user_profile' className='rounded-[50%] size-14 '/>
 
-      <div className='flex items-center gap-2'>
-        <DialogDemoTextArea props={{ nameOfProp: 'Description' }} />
-        <p>Add a small intro of youself .</p>
-      </div>
-
-      <div className='flex items-center gap-2'>
-        <DialogDemoSelect props={{ nameOfProp: 'Role' }} />
-        <p>
-          <span>Role: Backend</span>
-          <span className="text-[9px] font-normal pl-5">*Note: you only have 5 chances to change it.</span>
-        </p>
-      </div>
-
-      <div className='flex items-center justify-between'>
         <div className='flex items-center gap-2'>
-          <DialogDemoInput props={{ nameOfProp: 'Email' }} />
-          <p>msh17679@gmail.com</p>
+          <DialogDemoTextArea props={{ nameOfProp: 'Description' }} />
+          <p>Add a small intro of youself .</p>
         </div>
 
         <div className='flex items-center gap-2'>
-          <DialogDemoInput props={{ nameOfProp: 'Github' }} />
-          <p>https://github.com/mainshSharma1-dev</p>
+          <DialogDemoSelect props={{ nameOfProp: 'Role' }} />
+          <p>
+            <span>Role: Backend</span>
+            <span className="text-[9px] font-normal pl-5">*Note: you only have 5 chances to change it.</span>
+          </p>
         </div>
 
-        <div className='flex items-center gap-2'>
-          <DialogDemoInput props={{ nameOfProp: 'Linkedin' }} />
-          <p>https://linkedin/in/Manihsh11</p>
-        </div>
-      </div>
+        <div className='flex items-center justify-between'>
+          <div className='flex items-center gap-2'>
+            <DialogDemoInput props={{ nameOfProp: 'Email' }} />
+            <p>msh17679@gmail.com</p>
+          </div>
 
-      <div className='py-9 border-t flex flex-col gap-5'>
-        <p>Joined At: date</p>
-        <p>Team Created: 0</p>
-      </div>
+          <div className='flex items-center gap-2'>
+            <DialogDemoInput props={{ nameOfProp: 'Github' }} />
+            <p>https://github.com/mainshSharma1-dev</p>
+          </div>
+
+          <div className='flex items-center gap-2'>
+            <DialogDemoInput props={{ nameOfProp: 'Linkedin' }} />
+            <p>https://linkedin/in/Manihsh11</p>
+          </div>
+        </div>
+
+        <div className='py-9 border-t flex flex-col gap-5'>
+          <p>Joined At: date</p>
+          <p>Team Created: 0</p>
+        </div>
 
      </div>
    </div>
