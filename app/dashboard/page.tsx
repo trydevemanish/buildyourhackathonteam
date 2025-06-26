9b773435afcd731fe65ab874eac3e823bc077a40
manish sharma dev <manishvsharma1@gmail.com>
2025-06-26 03:17:40 -1200

    updated dashboard index page

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -17,6 +17,7 @@ export default function Page() {
     const handlefindUserCreatedTeamData = async() => {
       try {
         setLoading(true)
+        
         const res = await fetch('/api/fetchallteamCreatedbyuser')
   
         if(!res.ok){
@@ -42,48 +43,95 @@ export default function Page() {
 
   return (
     <main>
-      {teamDataFromBackend?.length > 0 ? 
-        <section>
-          <div className='flex justify-end items-center gap-3 px-3 border py-1'>
-            <button className='bg-purple-500 inline text-white px-8 py-[6px] text-xs rounded' onClick={() => router.push("/dashboard/createteam")}>
-              {/* <Link href="/dashboard/createteam"> */}
-                <span>+ create one </span>
-              {/* </Link> */}
-            </button>
-          </div> 
-          <div className='flex gap-5 px-8 py-2 overflow-y-auto scrollbar-hide'>
-            {
-              teamDataFromBackend.map((teamdata : TeamCardInfoType, idx : number) => (
-                <Teamcard props={teamdata} key={idx} />
-              ))
-            }
-          </div>
-        </section>
-        : 
+      {
+        loading ? 
         <div className='flex flex-col gap-3 justify-center items-center min-h-[calc(97vh-3rem)]'>
-          <Image src={Noteam} alt='noteam' className='w-60 h-40 animate-pulse'/>
-          <p className=''>No prev team 
-            <span className='text-purple-500'> found.</span>
-          </p>
-          <p className='opacity-70 text-xs'>Create new team to get started !</p>
-          {loading ? (
-            <div className="bg-black p-2 flex justify-center px-10 rounded ">
-              <LoaderIcon className="size-4 animate-spin stroke-white fill-white" />
-            </div>
-          ) : (
-            <button className="bg-black text-white px-8 py-1 text-xs rounded" onClick={() => router.push("/dashboard/createteam")}>
-              {/* <Link href="/dashboard/createteam"> */}
-                <p className="flex gap-2">
-                  <span>+</span>
-                  <span className="text-xs">Create Team</span>
-                </p> 
-              {/* </Link>  */}
-            </button>
-           )} 
+          <p className='opacity-70 text-xs animate-pulse'>Searching Your Previous Team....</p>
         </ div>
-       }
+        :
+        (
+          Array.isArray(teamDataFromBackend) && teamDataFromBackend.length > 0 ?
+          (
+            <section>
+              <div className='flex justify-end items-center gap-3 px-3 border py-1'>
+                <button className='bg-purple-500 inline text-white px-8 py-[6px] text-xs rounded' onClick={() => router.push("/dashboard/createteam")}>
+                  {/* <Link href="/dashboard/createteam"> */}
+                    <span>+ create one </span>
+                  {/* </Link> */}
+                </button>
+              </div> 
+              <div className='flex gap-5 px-8 py-2 overflow-y-auto scrollbar-hide'>
+                {
+                  teamDataFromBackend.map((teamdata : TeamCardInfoType, idx : number) => (
+                    <Teamcard props={teamdata} key={idx} />
+                  ))
+                }
+              </div>
+            </section>
+          ) :
+          (
+            <div className='flex flex-col gap-3 justify-center items-center min-h-[calc(97vh-3rem)]'>
+              {/* <Image src={Noteam} alt='noteam' className='w-60 h-40 animate-pulse'/> */}
+              <p className=''>No prev team 
+                <span className='text-purple-500'> found.</span>
+              </p>
+              <p className='opacity-70 text-xs'>Create new team to get started !</p>
+              <button className="bg-black text-white px-8 py-1 text-xs rounded" onClick={() => router.push("/dashboard/createteam")}>
+                {/* <Link href="/dashboard/createteam"> */}
+                  <p className="flex gap-2">
+                    <span>+</span>
+                    <span className="text-xs">Create Team</span>
+                  </p> 
+                {/* </Link>  */}
+              </button>
+            </ div>
+          )
+        )
+      }
     </main>
   )
 }
+
+// {teamDataFromBackend?.length > 0 ? 
+//         <section>
+//           <div className='flex justify-end items-center gap-3 px-3 border py-1'>
+//             <button className='bg-purple-500 inline text-white px-8 py-[6px] text-xs rounded' onClick={() => router.push("/dashboard/createteam")}>
+//               {/* <Link href="/dashboard/createteam"> */}
+//                 <span>+ create one </span>
+//               {/* </Link> */}
+//             </button>
+//           </div> 
+//           <div className='flex gap-5 px-8 py-2 overflow-y-auto scrollbar-hide'>
+//             {
+//               teamDataFromBackend.map((teamdata : TeamCardInfoType, idx : number) => (
+//                 <Teamcard props={teamdata} key={idx} />
+//               ))
+//             }
+//           </div>
+//         </section>
+//         : 
+        // <div className='flex flex-col gap-3 justify-center items-center min-h-[calc(97vh-3rem)]'>
+        //   <Image src={Noteam} alt='noteam' className='w-60 h-40 animate-pulse'/>
+        //   <p className=''>No prev team 
+        //     <span className='text-purple-500'> found.</span>
+        //   </p>
+        //   <p className='opacity-70 text-xs'>Create new team to get started !</p>
+        //   {loading ? (
+        //     <div className="bg-black p-2 flex justify-center px-10 rounded ">
+        //       <LoaderIcon className="size-4 animate-spin stroke-white fill-white" />
+        //     </div>
+        //   ) : (
+        //     <button className="bg-black text-white px-8 py-1 text-xs rounded" onClick={() => router.push("/dashboard/createteam")}>
+        //       {/* <Link href="/dashboard/createteam"> */}
+        //         <p className="flex gap-2">
+        //           <span>+</span>
+        //           <span className="text-xs">Create Team</span>
+        //         </p> 
+        //       {/* </Link>  */}
+        //     </button>
+        //    )} 
+        // </ div>
+//        }
+
 
 // dayegen507@mobilesm.com
